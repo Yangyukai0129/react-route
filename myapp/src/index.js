ddd550d9a30d31a7f0b3a61e450f024da41de852
kevin_yang <kevin_yang@comwave.com.tw>
2023-12-13 18:11:32 +0800

    login改寫寫法

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -19,7 +19,7 @@ import HostVanPricing from './pages/Host/HostVanPricing'
 import HostVanPhotos from './pages/Host/HostVanPhotos'
 import NotFound from './pages/NotFound';
 import Error from './component/Error'
-import Login, { loader as loginLoader } from './pages/Login'
+import Login, { loader as loginLoader, action as loginAction } from './pages/Login'
 import { requireAuth } from './util';
 
 import './server'
@@ -32,6 +32,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='login'
       element={<Login></Login>}
       loader={loginLoader}
+      action={loginAction}
     />
     {/*也可以這樣寫
             <Route path="vans">
--- myapp/src/pages/Login.jsx
+++ myapp/src/pages/Login.jsx
@@ -1,49 +1,78 @@
-import React from "react"
-import { useLoaderData } from "react-router-dom"
+import React, { useState } from "react"
+import { useLoaderData, useNavigate, Form } from "react-router-dom"
+import { loginUser } from "../api"
+
+export async function action({ request }) {
+    const formData = await request.formData()
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const data = await loginUser({ email, password })
+    console.log(data)
+    return null
+}
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
 }
 
 export default function Login() {
-    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    // const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const message = useLoaderData()
+    const [status, setStatus] = useState("idle")
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
-        console.log(e.target)
+        setError(null)
+            // console.log(loginFormData)
+            // console.log(e.target)
+            // loginUser(loginFormData)
+            // replace: true will cause the navigation to replace the current entry in the history stack instead of adding a new one
+            .then(data => navigate("/host", { replce: true }))
+            .catch(
+                err => { setError(err) }
+            )
+            .finally(setStatus("submitting"))
     }
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setLoginFormData(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+    // function handleChange(e) {
+    //     const { name, value } = e.target
+    //     setLoginFormData(prev => ({
+    //         ...prev,
+    //         [name]: value
+    //     }))
+    // }
 
     return (
         <div className="login-container">
             {message && <h3 className="red">{message}</h3>}
             <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className="login-form">
+            {error && <h3 className="red">{error.message}</h3>}
+            <Form method={"post"} className="login-form">
                 <input
                     name="email"
-                    onChange={handleChange}
+                    // onChange={handleChange}
                     type="email"
                     placeholder="Email address"
-                    value={loginFormData.email}
+                // value={loginFormData.email}
                 />
                 <input
                     name="password"
-                    onChange={handleChange}
+                    // onChange={handleChange}
                     type="password"
                     placeholder="Password"
-                    value={loginFormData.password}
+                // value={loginFormData.password}
                 />
-                <button>Log in</button>
-            </form>
+                <button disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging..."
+                        : "Log in"
+                    }
+                </button>
+            </Form>
         </div>
     )
 }
